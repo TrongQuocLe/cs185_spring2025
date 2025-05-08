618feb1a3b93f218500d1f55e081064e07d7285a
TrongQuocLe <tqle.email@gmail.com>
2025-05-08 01:34:30 -0700

    update

--- project_GoM/code/SIZE.h
+++ project_GoM/code/SIZE.h
@@ -44,14 +44,14 @@ C     Nr  :: No. points in Z for full process domain.
       INTEGER Ny
       INTEGER Nr
       PARAMETER (
-     &           sNx =  40,
-     &           sNy =  50,
+     &           sNx =  100,
+     &           sNy =  80,
      &           OLx =   4,
      &           OLy =   4,
      &           nSx =   1,
      &           nSy =   1,
-     &           nPx =  2,
-     &           nPy =  2,
+     &           nPx =  4,
+     &           nPy =  4,
      &           Nx  = sNx*nSx*nPx,
      &           Ny  = sNy*nSy*nPy,
      &           Nr  =  50 )
